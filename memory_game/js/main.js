f5c1c119b1c2119183b7b0a06e95dbb8212be3ee
Gabriel Naughton <gabrielnaughton@Gabriels-MacBook-Air.local>
2019-02-28 12:04:20 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,25 @@
-console.log("Up and running!");
 
-var cards = ["queen","queen","king","king"];
+
+
+var cards = ["queen", "queen", "king", "king"];
+
 var cardsInPlay = [];
-var checkForMatch = function (){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+	alert("You found a match!");
 } else {
-console.log("Sorry, try again.");
-};
-};
-var flipCard = function(cardId){
-	checkForMatch();
-if (cardsInPlay.length === 2){
-	if(cardsInPlay[0]===cardsInPlay[1]){
-		alert("You found a match!");
-	} else{
-		alert("Sorry, try again.");
-	}}
-	console.log("User flipped " + cards[cardId]);
+	alert("Sorry, try again.");
+}
+}
+
+var flipCard = function (cardId) {
+	console.log("User flipped" + cards[cardId]);
 	cardsInPlay.push(cards[cardId]);
+
+if (cardsInPlay.length === 2){
+	checkForMatch();
+}
 }
 flipCard(0);
 flipCard(2);
